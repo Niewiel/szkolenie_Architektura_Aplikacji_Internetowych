6f172ae456bceb9f4fd77511d8e1065d2271cab4
niewiel <tniewiel@gmail.com>
2018-06-09 15:42:21 +0200

    Initial commit

---
+++ js/interpreter.js
@@ -0,0 +1,38 @@
+var Westeros;
+(function (Westeros) {
+    var History;
+    (function (History) {
+        class Battle {
+            constructor(battleGround, agressor, deffender, victor) {
+                this.battleGround = battleGround;
+                this.agressor = agressor;
+                this.deffender = deffender;
+                this.victor = victor;
+            }
+        }
+
+        History.Battle = Battle;
+
+        class Parser {
+            constructor(battleText) {
+                this.battleText = battleText;
+                this.currentIndex = 0;
+                this.battleList = battleText.split("\n");
+            }
+
+            nextBattle() {
+                if (!this.battleList[0]) {
+                    return null;
+                }
+                var segments = this.battleList[0].match(/\((.+?)\s?->\s?(.+?)\s?<-\s?(.+?)\)\s?->\s?(.+?)/);
+                return new Battle(segments[2], segments[1], segments[3], segments[4]);
+            }
+        }
+
+        History.Parser = Parser;
+    })(History = Westeros.History || (Westeros.History = {}));
+})(Westeros || (Westeros = {}));
+
+var bitwa = '(Mama -> Dom <- Michas) -> Michas';
+var p = new Westeros.History.Parser(bitwa);
+console.log(p.nextBattle());
