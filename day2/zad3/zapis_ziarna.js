8b2ba91465a886f5942df87bca63580b50800317
niewiel <tniewiel@gmail.com>
2018-06-10 14:26:54 +0200

    node2

---
+++ day2/zad3/zapis_ziarna.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var ziarna = ['pszenica', 'ryż', 'owies', 'żyta', 'quinoa'];
+var options = {encoding: 'UTF-8', flag: 'w'};
+var fileWriteStream = fs.createWriteStream("ziarna.txt", options);
+fileWriteStream.on("close", function () {
+    console.log("zamknięto plik");
+
+})
+while (ziarna.length) {
+    var dane = ziarna.pop() + " ";
+    fileWriteStream.write(dane);
+    console.log("zapisano %s", dane);
+
+}
+fileWriteStream.end();
